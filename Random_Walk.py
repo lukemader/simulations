8a6d564a8dd018ff6e38066815758a378241b3c1
lukemader <lukemader@protonmail.com>
2022-01-14 23:37:29 +0000

    Update Random_Walk.py

--- Random_Walk.py
+++ Random_Walk.py
@@ -1,8 +1,12 @@
 import random
+import re
+import pickle
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Random_Walk():
     """class Random_Walk
-    Class to model a simple random walk.
+    Class to model a discrete random walk.
     Features:
         - Simulate random walk based on position, possible moves, and probability distribution of moves.
     """
@@ -85,3 +89,109 @@ class Random_Walk():
         step = self.step(p)
         path = [next(step) for i in range(self.number_of_steps)]
         return (path)
+
+    def graph(self, path = None, name = "random_walk.jpg", save_pickle = False):
+        """class method Random_Walk.graph
+        Method to call correct graphing function
+        PARAMETERS:
+            - path:                 float 2D array path
+                - if none: assume that user wants default path self.path
+            - file name:            string name
+                - if none, call "random_walk.jpg"
+            - create pickle file?:  bool save_pickle
+                - if none, assume False
+            RETURN:
+                - None
+                - Calls correct graphing function, which will return True and save image file of graph
+        """
+        # input handling:
+        # acceptable name extensions: jpg. jpeg, png
+        # if name has wrong extension type, get rid of extension type
+        # if name has no extension type, no issue as matplotlib will default to png
+        # if no path specified, set to default path
+        if not name.endswith(('jpg', 'jpeg', 'png')):
+            if "." in name:
+                name = name.split('.', 1)[0]
+        if path == None: path = self.path
+
+        # call graphing function based on if 1D, 2D or 3D starting position
+        if len(self.starting_position) == 1:
+            # 1D is actually just 2D with x axis set to discrete time stamps
+            # so create time stamps and plot
+            x = [i for i in range(0, self.number_of_steps)]
+            path = [[x[i], path[i]] for i in range(self.number_of_steps)]
+            self.graph_2D(path, name, save_pickle)
+        elif len(self.starting_position) == 2:
+            self.graph_2D(path, name, save_pickle)
+        elif len(self.starting_position) == 3:
+            self.graph_3D(path, name, save_pickle)
+        else:
+            raise ValueError('Starting position must be 1-dimensional, 2-dimensional or '
+                             '3-dimensional for a graph of the walk to be generated.')
+
+    def graph_2D(self, path, name, save_pickle):
+        """class method Random_Walk.graph_2D
+        Used by: Random_Walk.graph
+        Method to create image file of graph of 2D walk
+        PARAMETERS:
+            - path:         float 2D array path
+            - name:         string name
+            - save_pickle:  bool
+        RETURN:
+            - True
+            - Saves 2D plot of path and potentially saves pickle of plot
+        """
+        # extract x and y values from each position in path
+        x = [position[0] for position in path]
+        y = [position[1] for position in path]
+
+        #plot and save plot
+        plt.plot(x, y)
+        plt.savefig(name)
+
+        if save_pickle == True:
+            # if user wants to save pickle of plot,
+            # get rid of extension from name,
+            # add correct pickle extension
+            # extract the figure and save
+            if "." in name:
+                name = name.split('.', 1)[0]
+            name += '.fig.pickle'
+            fig = plt.figure()
+            pickle.dump(fig, open(name, 'wb'))
+
+        return True
+    def graph_3D(self, path, name, save_pickle):
+        """class method Random_Walk.graph_3D
+            Used by: Random_Walk.graph
+            Method to create image file of graph of 3D walk
+            PARAMETERS:
+                - path:         float 2D array path
+                - name:         string name
+                - save_pickle:  bool
+            RETURN:
+                - True
+                - Saves 3D plot of path and potentially saves pickle of plot
+        """
+        # extract x,y, and z values from each position in plot
+        x = [position[0] for position in path]
+        y = [position[1] for position in path]
+        z = [position[2] for position in path]
+
+        # 3d plot and save plot
+        fig = plt.figure()
+        ax = plt.axes(projection="3d")
+        ax.plot(x, y, z)
+        plt.savefig(name)
+
+        if save_pickle == True:
+            # if user wants to save pickle of plot,
+            # get rid of extension from name,
+            # add correct pickle extension
+            # save figure
+            if "." in name:
+                name = name.split('.', 1)[0]
+            name += '.fig.pickle'
+            pickle.dump(fig, open(name, 'wb'))
+
+        return True
